98c5f533c4ecdde68f72fa222304ae03b0b1260b
Henning Cullin <henningcullin@gmail.com>
2024-04-27 20:06:47 +0200

    feat: FromRow implementation working with joins and overlapping properties

--- pql.php
+++ pql.php
@@ -3,9 +3,16 @@
 class FromRow {
     public function __construct(array $row)
     {
-        foreach ($row as $key => $value) {
+        $this_class = strtolower(static::class);
+        foreach ($row[$this_class] as $key => &$value) {
             if (property_exists($this, $key)) {
                 $this->$key = $value;
+            }
+        }
+        unset($row[$this_class]);
+        foreach($row as $class => &$contents) {
+            if (property_exists($this, $class)) {
+                $this->$class = new $class($row);
             }
         }
     }
@@ -25,6 +32,7 @@ class pql {
     }
 
     public static function query_as($class, string $query, ...$params) {
+        $query = self::parse_query($query);
         $instance = new self();
         $instance->class = $class;
         $instance->query = $query;
@@ -57,24 +65,51 @@ class pql {
         return $value;
     }
 
+    private static function add_aliases($query) {
+        $query_parts = preg_split('/from/i', $query, 2);
+
+        // Regular expression to match column names
+        $column_pattern = '/(\w+)\.(\w+)/';
+    
+        // Find all matches in the query
+        preg_match_all($column_pattern, $query_parts[0], $matches);
+    
+        // Create an associative array to store column aliases
+        $alias_dict = [];
+    
+        // Generate column aliases
+        foreach ($matches[0] as $match) {
+            list($table, $column) = explode('.', $match);
+            $alias = "{$table}.{$column}";
+            $alias_dict[$match] = "{$alias} AS {$table}_{$column}";
+        }
+    
+        // Replace column names with aliases in the query
+        foreach ($alias_dict as $original => $alias) {
+            $query_parts[0] = str_replace($original, $alias, $query_parts[0]);
+        }
+    
+        $query = implode('FROM', $query_parts);
+
+        return $query;
+    }
+
+    private static function parse_query($query) {
+        if (str_contains($query, '.')) {
+            $query = self::add_aliases($query);
+        }
+        return $query;
+    }
+
     private static function parse_row($row) {
         $new_row = array();
-
-        if (!is_array($row)) return array();
         
-        for ($i = 0; $i < count($row); $i += 2) {
-            $index_key = array_keys($row)[$i];
-            $name_key = isset(array_keys($row)[$i + 1]) ? array_keys($row)[$i + 1] : null;
+        if (!is_array($row)) return $new_row;
 
-            if (is_numeric($name_key)) {
-                $i -= 1;
-                continue;
-            }
-
-            $new_row[$name_key] = $row[$index_key];
+        foreach ($row as $key => $val) {
+            $key_arr = explode('_', $key);
+            $new_row[$key_arr[0]][$key_arr[1]] = $val;
         }
-
-        var_dump($new_row);
 
         return $new_row;
     }
@@ -88,7 +123,7 @@ class pql {
 
         $result = $stmt->execute();
 
-        $first_row = self::parse_row($result->fetchArray());
+        $first_row = $result->fetchArray(SQLITE3_ASSOC);
 
         if (empty($first_row)) {
             return null;
@@ -98,7 +133,7 @@ class pql {
 
         $rows = array();
 
-        while ($row = self::parse_row($result->fetchArray())) {
+        while ($row = self::parse_row($result->fetchArray(SQLITE3_ASSOC))) {
             if (isset($this->class)) $rows[] = new $this->class($row);
             else $rows[] = $row;
         }
