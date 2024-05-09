1e25cb00b893bd6b29b9e4985f8fd0b1d7f031a8
Henning Cullin <henningcullin@gmail.com>
2024-05-09 11:54:28 +0200

    refactor: move setup code to separate file

--- index.php
+++ index.php
@@ -23,42 +23,7 @@ class Machine extends FromRow {
     public string $make;
 }
 
-/* public function __construct($row)
-{
-    $this->id = $row['machine'];
-    $this->name = $row['name'];
-    $this->make = $row['make'];
-} */
-
 $conn = new SQLite3($dir . $file);
-
-/* $result = $conn->query('CREATE TABLE IF NOT EXISTS machine (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255),
-    make VARCHAR(255)
-);');
-
-$result = $conn->query('CREATE TABLE IF NOT EXISTS task (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(255),
-    description TEXT,
-    machine INTEGER
-);'); */
-
-/* $result = $conn->query('INSERT INTO machine (name, make) VALUES("V60", "Volvo")');
-$result = $conn->query('INSERT INTO machine (name, make) VALUES("V90", "Volvo")');
-$result = $conn->query('INSERT INTO machine (name, make) VALUES("Corolla", "Toyota")');
-$result = $conn->query('INSERT INTO machine (name, make) VALUES("Escort", "Ford")');
-$result = $conn->query('INSERT INTO machine (name, make) VALUES("Golf", "Volkswagen")');
-$result = $conn->query('INSERT INTO machine (name, make) VALUES("Passat", "Volkswagen")'); */
-
-/* $result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Very Urget", "Replace Engine Oil", 4)');
-$result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Quite Urget", "Fix Aux Cable", 2)');
-$result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Not urgent", "Refill blinker fluid", 1)'); */
-
-/* $result = pql::query(
-    'SELECT * FROM task LEFT JOIN machine ON task.machine = machine.id'
-)->fetch_all($conn); */
 
 $result = pql::query_as(
     Task::class,
---
+++ setup.php
@@ -0,0 +1,24 @@
+<?php
+/* $result = $conn->query('CREATE TABLE IF NOT EXISTS machine (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255),
+    make VARCHAR(255)
+);');
+
+$result = $conn->query('CREATE TABLE IF NOT EXISTS task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(255),
+    description TEXT,
+    machine INTEGER
+);'); */
+
+/* $result = $conn->query('INSERT INTO machine (name, make) VALUES("V60", "Volvo")');
+$result = $conn->query('INSERT INTO machine (name, make) VALUES("V90", "Volvo")');
+$result = $conn->query('INSERT INTO machine (name, make) VALUES("Corolla", "Toyota")');
+$result = $conn->query('INSERT INTO machine (name, make) VALUES("Escort", "Ford")');
+$result = $conn->query('INSERT INTO machine (name, make) VALUES("Golf", "Volkswagen")');
+$result = $conn->query('INSERT INTO machine (name, make) VALUES("Passat", "Volkswagen")'); */
+
+/* $result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Very Urget", "Replace Engine Oil", 4)');
+$result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Quite Urget", "Fix Aux Cable", 2)');
+$result = $conn->query('INSERT INTO task (title, description, machine) VALUES("Not urgent", "Refill blinker fluid", 1)'); */
